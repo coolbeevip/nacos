d0f9690d41737121631f5b5525864361d84444ec
Lei Zhang <zhanglei@apache.org>
2024-11-08 10:36:31 +0800

    Add ConsoleUIBlockFilter

--- console/src/main/java/com/alibaba/nacos/console/filter/ConsoleUIBlockFilter.java
+++ console/src/main/java/com/alibaba/nacos/console/filter/ConsoleUIBlockFilter.java
@@ -1,19 +1,20 @@
 package com.alibaba.nacos.console.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 
 public class ConsoleUIBlockFilter extends OncePerRequestFilter {
-    private final boolean consoleUiEnabled;
-
-    public ConsoleUIBlockFilter(boolean consoleUiEnabled) {
-        this.consoleUiEnabled = consoleUiEnabled;
-    }
+    private final Logger log = LoggerFactory.getLogger(ConsoleUIBlockFilter.class);
+    private boolean consoleUiEnabled = true;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -24,5 +25,22 @@ public class ConsoleUIBlockFilter extends OncePerRequestFilter {
             return;
         }
         filterChain.doFilter(request, response);
+    }
+
+    @Scheduled(fixedRate = 60000)
+    public void checkUIStatus() {
+        File file = new File("DISABLE_UI.txt");
+        boolean disable_flag = file.exists();
+        if (disable_flag) {
+            if (this.consoleUiEnabled){
+                log.info("Console UI is off");
+            }
+            this.consoleUiEnabled = false;
+        } else {
+            if (!this.consoleUiEnabled){
+                log.info("Console UI is on");
+            }
+            this.consoleUiEnabled = true;
+        }
     }
 }
